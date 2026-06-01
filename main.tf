d55aee7d82ae9ec08941c6359b677ce8e7e9a164
Naveen Kumar <marapareddynaveen678@gmail.com>
2026-06-01 22:36:03 +0530

    actions

--- main.tf
+++ main.tf
@@ -19,6 +19,7 @@ resource "aws_subnet" "main" {
   count = length(var.public_subnet_cidrs)
   vpc_id     = aws_vpc.main.id  
   cidr_block = var.public_subnet_cidrs[count.index]
+  map_public_ip_on_launch = true
 
   tags = var.public_subnet_tags
 }
