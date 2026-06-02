506b46011526dd73e4b3b97a16925e669e3041e7
Naveen Kumar <marapareddynaveen678@gmail.com>
2026-06-02 11:32:38 +0530

    actions

--- locals.tf
+++ locals.tf
@@ -21,8 +21,9 @@ locals {
     public_subnet_tags = merge(
         local.common_tags,
         {
-            Name = "${var.project}-${var.environment}"
+            Name = "${var.project}-${var.environment}-public-${local.az_names[count.index]}"
         },
         var.public_subnet_tags,
     )
+    az-names = slice(data.aws_availability_zones.available.names, 0, 2)
 }
