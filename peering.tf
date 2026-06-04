73e37b97ad6c3dbdb549e3c266214878fd4427b3
Naveen Kumar <marapareddynaveen678@gmail.com>
2026-06-04 20:21:12 +0530

    actions

--- peering.tf
+++ peering.tf
@@ -20,7 +20,7 @@ resource "aws_vpc_peering_connection" "default" {
 
    tags = merge(
     local.common_tags,
-    # roboshop=dev
+    # roboshop=dev-default
 
     {
       Name = "${var.project}-${var.environment}-default"
