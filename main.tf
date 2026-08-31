156f0322b45b41b00e1c36ea437566742da6c841
Naveen Kumar <marapareddynaveen678@gmail.com>
2026-08-31 09:42:55 +0530

    actions

--- main.tf
+++ main.tf
@@ -177,5 +177,15 @@ resource "aws_route_table_association" "database" {
   route_table_id = aws_route_table.database.id
 }
 
+resource "aws_db_subnet_group" "roboshop" {
+  name       = "${var.project}-${var.environment}"
+  subnet_ids = [aws_subnet.database[0].id,aws_subnet.database[1].id]
 
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}"
+    }
+  )
+}
 
